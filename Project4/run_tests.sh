6b0cd459b50cf11cd408f9c28660c9428e0f056a
Stephen Paslaski <paslasks@gmail.com>
2014-10-10 08:20:34 -0400

    Fix leftover ref.tic problem

--- Project4/run_tests.sh
+++ Project4/run_tests.sh
@@ -30,6 +30,7 @@ function run_error_test {
     if [ -e ref.tic ] ; then
 	    Test_Suite/TubeIC $project.tic > $project.out
 	    Test_Suite/TubeIC ref.tic > ref.out
+	    rm  ref.tic $project.tic;
 	    diff -w ref.out $project.out;
 	    result=$?;
 	    rm $project.out ref.out;
